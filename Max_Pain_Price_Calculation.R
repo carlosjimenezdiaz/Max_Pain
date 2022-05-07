22a1fafad52a97e0369b92d295b40b77b247a70a
carlosjimenezdiaz <carlos.jimenezdiaz@protonmail.com>
2022-05-07 11:34:21 +0200

    Fixing Yahoo API

--- Max_Pain_Price_Calculation.R
+++ Max_Pain_Price_Calculation.R
@@ -15,8 +15,11 @@ libraries()
 Ticker              <- "^SPX" # Defining the ticker
 Ticker_Multi        <- 100   # Multiplier of the Option Contract
 environment_r       <- "EU"  # Where is your computer located? EU or US
-Date_MP_Calculation <- "2022-04-29" # Expiration that you want to analyze
+Date_MP_Calculation <- "2022-05-13" # Expiration that you want to analyze
 Ticker_Label        <- str_replace_all(Ticker, "[^[:alnum:]]", " ") %>% str_replace_all(.,"[ ]+", "")
+
+# Getting Current Price
+Current_Price <- getQuote(Ticker)$Last
 
 # Getting future option chain
 db_option_chain <- getOptionChain(Ticker, src = "yahoo", Exp = str_glue("{lubridate::year(Sys.Date())}/{lubridate::year(Sys.Date()) + 1}")) %>%
@@ -24,62 +27,31 @@ db_option_chain <- getOptionChain(Ticker, src = "yahoo", Exp = str_glue("{lubrid
   enframe() %>%
   unnest(cols = c(value))
 
-# Addaping the structure of the option chain to US or EU machines
-if(environment_r == "US"){
-  # Fixing some formats
+  # Extracting the type of Option
+  opt_type <- c()
+  for(i in 1:nrow(db_option_chain)){ # i <- 1
+    
+    # Selecting the name column
+    name_tag <- db_option_chain$name[i]
+    
+    # Getting the length of that name
+    name_length <- nchar(name_tag)
+    
+    # Getting the option type
+    opt_type <- c(opt_type, substr(name_tag, 14, name_length))
+  }
+  
+# Adding the Option Type
   db_option_chain <- db_option_chain %>%
-    dplyr::mutate(data_month = substr(name, 1, 3),  # Extracting just the portion related to the month
-                  data_day   = substr(name, 5, 6),  # Extracting just the portion related to the day
-                  data_year  = substr(name, 8, 11), # Extracting just the portion related to the year
-                  type       = substr(name, 13, length(name)),
-                  data_month = case_when(data_month == "Jan" ~ "01",
-                                         data_month == "Feb" ~ "02",
-                                         data_month == "Mar" ~ "03",
-                                         data_month == "Apr" ~ "04",
-                                         data_month == "May" ~ "05",
-                                         data_month == "Jun" ~ "06",
-                                         data_month == "Jul" ~ "07",
-                                         data_month == "Aug" ~ "08",
-                                         data_month == "Sep" ~ "09",
-                                         data_month == "Oct" ~ "10",
-                                         data_month == "Nov" ~ "11",
-                                         data_month == "Dec" ~ "12",
-                                         TRUE ~ data_month),
-                  expiration = as.Date(str_glue("{data_year}-{data_month}-{data_day}"), format = "%Y-%m-%d")) %>%
-    dplyr::select(expiration, type, Strike, OI) %>% 
-    replace(is.na(.), 0)
-}else{
-  # Fixing some formats
-  db_option_chain <- db_option_chain %>%
-    dplyr::mutate(data_month = substr(name, 1, 3),  # Extracting just the portion related to the month
-                  data_day   = substr(name, 6, 7),  # Extracting just the portion related to the day
-                  data_year  = substr(name, 9, 12), # Extracting just the portion related to the year
-                  type       = substr(name, 14, length(name)),
-                  data_month = case_when(data_month == "ene" ~ "01",
-                                         data_month == "feb" ~ "02",
-                                         data_month == "mar" ~ "03",
-                                         data_month == "abr" ~ "04",
-                                         data_month == "may" ~ "05",
-                                         data_month == "jun" ~ "06",
-                                         data_month == "jul" ~ "07",
-                                         data_month == "ago" ~ "08",
-                                         data_month == "sep" ~ "09",
-                                         data_month == "oct" ~ "10",
-                                         data_month == "nov" ~ "11",
-                                         data_month == "dic" ~ "12",
-                                         TRUE ~ data_month),
-                  expiration = as.Date(str_glue("{data_year}-{data_month}-{data_day}"), format = "%Y-%m-%d")) %>%
-    dplyr::select(expiration, type, Strike, OI) %>%  
-    replace(is.na(.), 0)
-}
-
+  dplyr::mutate(type = opt_type)
+  
 ################################################
 # Calculating the Strike where the Max Pain is #
 ################################################
 
 # Option Interest distribution
 p <- db_option_chain %>%
-  dplyr::filter(expiration == Date_MP_Calculation) %>%
+  dplyr::filter((Expiration %>% as.Date()) == (Date_MP_Calculation %>% as.Date())) %>%
   ggplot(aes(x = Strike, y = OI)) +
   geom_bar(stat = "identity") +
   scale_y_continuous(labels = scales::dollar_format(prefix = "")) +
@@ -96,7 +68,7 @@ p
 ggsave("OI_Distribution_Option_Type.png", plot = p, device = "png", path = "Plots/", width = 25, height = 15, units = "cm")
 
 p <- db_option_chain %>%
-  dplyr::filter(expiration == Date_MP_Calculation) %>%
+  dplyr::filter((Expiration %>% as.Date()) == (Date_MP_Calculation %>% as.Date())) %>%
   ggplot(aes(x = Strike, y = OI, colour = type)) +
   geom_line() +
   scale_y_continuous(labels = scales::dollar_format(prefix = "")) +
@@ -114,16 +86,17 @@ ggsave("OI_Distribution_Option_Type_V2.png", plot = p, device = "png", path = "P
 # Analyzing the Calls
 db_Call_Cash <- NULL
 db_Calls_MP  <- db_option_chain %>%
-  dplyr::filter(expiration == Date_MP_Calculation) %>% 
+  dplyr::filter((Expiration %>% as.Date()) == (Date_MP_Calculation %>% as.Date())) %>%
   dplyr::filter(type == "calls")
 
-for(closing_price in db_Calls_MP$Strike){ # closing_price <- 195
+for(closing_price in db_Calls_MP$Strike){ # closing_price <- 3900
   
   # Intrinsic Value calculation (because at expiration extrinsic value is ZERO)
   IV_Calculation <- db_Calls_MP %>%
     dplyr::transmute(status          = ifelse(closing_price >= Strike, "ITM", "OTM"),
                      Intrinsic_Value = case_when(status == "ITM" ~ closing_price - Strike, TRUE ~ 0),
-                     Cash_Value      = Intrinsic_Value*OI*Ticker_Multi)
+                     Cash_Value      = Intrinsic_Value*OI*Ticker_Multi) %>%
+    replace(is.na(.), 0)
   
   # Collecting the results
   if(is.null(db_Call_Cash)){
@@ -139,16 +112,17 @@ for(closing_price in db_Calls_MP$Strike){ # closing_price <- 195
 # Analyzing the Puts
 db_Put_Cash <- NULL
 db_Puts_MP  <- db_option_chain %>%
-  dplyr::filter(expiration == Date_MP_Calculation) %>% 
+  dplyr::filter((Expiration %>% as.Date()) == (Date_MP_Calculation %>% as.Date())) %>%
   dplyr::filter(type == "puts")
 
 for(closing_price in db_Puts_MP$Strike){ # closing_price <- 175
   
   # Intrinsic Value calculation (because at expiration extrinsic value is ZERO)
-  IV_Calculation <- data.frame(Strike = db_Puts_MP$Strike) %>%
+  IV_Calculation <- db_Puts_MP %>%
     dplyr::transmute(status          = ifelse(closing_price <= Strike, "ITM", "OTM"),
                      Intrinsic_Value = case_when(status == "ITM" ~ Strike - closing_price, TRUE ~ 0),
-                     Cash_Value      = Intrinsic_Value*db_Puts_MP$OI*Ticker_Multi)
+                     Cash_Value      = Intrinsic_Value*OI*Ticker_Multi) %>%
+    replace(is.na(.), 0)
   
   # Collecting the results
   if(is.null(db_Put_Cash)){
@@ -181,7 +155,7 @@ p <- db_Max_Pain %>%
   ggplot(aes(x = Price, y = Total_Cash_Value)) +
   geom_line() +
   scale_y_continuous(labels = scales::dollar_format()) +
-  labs(title    = str_glue("Max Pain Calculations - Expected Pin Price at expiration: {Price_Max_Pain}"),
+  labs(title    = str_glue("Max Pain Calculations - Expected Pin Price at expiration: {Price_Max_Pain}. Currently Diff {round((Price_Max_Pain/Current_Price - 1)*100, digits = 2)}%."),
        subtitle = str_glue("Analysis performed on {Ticker_Label} - Expiration Date: {Date_MP_Calculation}"),
        caption  = "By: Carlos Jimenez",
        x = "Underlying Price",
